398f724badf41ee104125976b4aa72b2f2d65622
ivanv3lkov <ivanv3lkov@gmail.com>
2020-09-10 18:25:29 +0300

    'Homework'

--- HTML, CSS and JS Homework/static/main.js
+++ HTML, CSS and JS Homework/static/main.js
@@ -1,16 +1,16 @@
-let container = document.querySelector('#container');
-let errors = document.querySelector('#error')
-let showBtn = document.querySelector('button')
+function makeElement(type, text) {
+    let element = document.createElement(type);
+    if (text !== null) {
+        element.textContent = text;
+    }
+    return element;
+}
+
+const container = document.querySelector('#container');
+const errorContainer = document.querySelector('#errorContainer');
+const showBtn = document.querySelector('#showBtn');
 
 showBtn.addEventListener('click', (e) => {
-    function makeEl(type, text) {
-        let el = document.createElement(type);
-        if (text !== null) {
-            el.textContent = text;
-        }
-        return el;
-    }
-
     e.preventDefault();
     let location = document.querySelector('input').value.trim();
 
@@ -19,40 +19,51 @@ showBtn.addEventListener('click', (e) => {
         document.querySelector('input').style.border = '';
     }
 
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=27246259aabc1b11ce70b1b84bb09bf4`)
-        .then(function (response) {
-            errors.textContent = '';
-            
-            let country = response.data.sys.country;
-            let h2 = makeEl('h2', location + `, ${country}`);
-            let temp = makeEl('p', `Temperature: ${(response.data.main.temp - 273.15).toFixed(2)}°C`);
-            let feelsLike = makeEl('p', `Feels Like: ${(response.data.main.feels_like - 273.15).toFixed(2)}°C`);
-            let humidity = makeEl('p', `Humidity: ${response.data.main.humidity}%`);
-            let pressure = makeEl('p', `Pressure: ${response.data.main.pressure} hPa`);
-            let temp_max = makeEl('p', `Max Temperature: ${(response.data.main.temp_max - 273.15).toFixed(2)}°C`);
-            let temp_min = makeEl('p', `Min Temperature: ${(response.data.main.temp_min - 273.15).toFixed(2)}°C`);
-            let wind = makeEl('p', `Wind: ${response.data.wind.speed} kmh`);
+    if (isNaN(location) && location !== undefined) {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=37c689d8c93066153ad764c1c8ca9a0d`)
+            .then(function (response) {
+                errorContainer.textContent = '';
 
-            let section = makeEl('section');
+                let country = response.data.sys.country;
+                let h2 = makeElement('h2', location + `, ${country}`);
+                let temp = makeElement('p', `Temperature: ${(response.data.main.temp - 273.15).toFixed(2)}°C`);
+                let feelsLike = makeElement('p', `Feels Like: ${(response.data.main.feels_like - 273.15).toFixed(2)}°C`);
+                let humidity = makeElement('p', `Humidity: ${response.data.main.humidity}%`);
+                let pressure = makeElement('p', `Pressure: ${response.data.main.pressure} hPa`);
+                let temp_max = makeElement('p', `Max Temperature: ${(response.data.main.temp_max - 273.15).toFixed(2)}°C`);
+                let temp_min = makeElement('p', `Min Temperature: ${(response.data.main.temp_min - 273.15).toFixed(2)}°C`);
+                let wind = makeElement('p', `Wind: ${response.data.wind.speed} kmh`);
 
-            section.appendChild(h2);
-            section.appendChild(temp);
-            section.appendChild(feelsLike);
-            section.appendChild(humidity);
-            section.appendChild(pressure);
-            section.appendChild(temp_max);
-            section.appendChild(temp_min);
-            section.appendChild(wind);
-            container.appendChild(section);
-        }).catch(function () {
-            if (location) {
-                let p = document.createElement('p');
-                p.innerText = 'Sorry, not found.'
-                errors.appendChild(p);
-            } else {
-                document.querySelector('input').style.border = 'solid red';
-            }
-        })
+                let section = makeElement('section');
 
+                section.appendChild(h2);
+                section.appendChild(temp);
+                section.appendChild(feelsLike);
+                section.appendChild(humidity);
+                section.appendChild(pressure);
+                section.appendChild(temp_max);
+                section.appendChild(temp_min);
+                section.appendChild(wind);
+                container.appendChild(section);
+            }).catch(function () {
+                if (location) {
+                    let p = makeElement('p', 'Sorry, not found.');
+                    errorContainer.appendChild(p);
+                } else {
+                    document.querySelector('input').style.border = 'solid red';
+                }
+            })
+
+        document.querySelector('input').value = '';
+        document.querySelector('input').style.border = '';
+    } else {
+        if (location) {
+            let p = makeElement('p', 'Please enter location, not numbers!');
+            errorContainer.appendChild(p);
+            document.querySelector('input').style.border = '';
+        } else {
+            document.querySelector('input').style.border = 'solid red';
+        }
+    }
     document.querySelector('input').value = '';
-})
+});
